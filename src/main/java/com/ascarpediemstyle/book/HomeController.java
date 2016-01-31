f2c7a933d8496e542dc3c123df59f06b0702feb8
M.Iwasaki <miwasaki@MIwasaki-no-MacBook-Pro.local>
2016-02-01 03:29:23 +0900

    tiles, thyemleaf

--- src/main/java/com/ascarpediemstyle/book/HomeController.java
+++ src/main/java/com/ascarpediemstyle/book/HomeController.java
@@ -3,9 +3,11 @@ package com.ascarpediemstyle.book;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,11 +16,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
+
 @Controller
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
+	@Autowired
+    private Properties applicationProperties;
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
@@ -26,6 +31,11 @@ public class HomeController {
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
+		String message = 
+                applicationProperties.getProperty("app.test");
+        logger.info("message : " + message);
+ 
+        
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
